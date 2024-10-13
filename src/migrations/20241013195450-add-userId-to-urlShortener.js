870aabfc3284ddc22761cd991def9e8e4c3c312d
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2024-10-13 17:18:08 -0300

    ✨ feat(sequelize): config sequelize to run migrations

---
+++ src/migrations/20241013195450-add-userId-to-urlShortener.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('shortened_url', 'id_user', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('shortened_url', 'id_user');
+  }
+};
